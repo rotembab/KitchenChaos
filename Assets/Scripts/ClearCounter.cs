9615bb239ad95aeb81657271196f9609390927f2
Rotem Babani <rotembabmen@gmail.com>
2025-02-09 19:45:44 +0200

    Added Cheese Spawn with Tomatos!

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform CounterTopPoint;
     public void Interact()
     {
-        Debug.Log("Interacting with counter");
+        Debug.Log("Interact!");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, CounterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero; 
     }
 }
