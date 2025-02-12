1aee19193e46cc72a66c4b4185133b526ff2b4db
Rotem Babani <rotembabmen@gmail.com>
2025-02-12 14:25:26 +0200

    Added pickup and drop on clear container

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,8 +5,25 @@ public class ClearCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    public override void Interact(Player player)
+    public override void Interact(Player player) 
     {
-
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+                
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 } 
