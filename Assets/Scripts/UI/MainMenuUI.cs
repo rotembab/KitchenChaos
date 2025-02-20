1bc376f0ddc321717ecb1230542c3c15ca569c57
Rotem Babani <rotembabmen@gmail.com>
2025-02-20 09:45:52 +0200

    Adding main menu scene and buttons

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button quitBtn;
+    
+    private void a
+}
