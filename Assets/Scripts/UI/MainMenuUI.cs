c49f61e3e9c84f584bc689b03db7262435e222bc
Rotem Babani <rotembabmen@gmail.com>
2025-02-21 11:06:11 +0200

    Added main menu and loading scene

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,10 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button playBtn;
     [SerializeField] private Button quitBtn;
-    
-    private void a
+
+    private void Awake()
+    {
+        playBtn.onClick.AddListener(() =>
+        {
+           Loader.LoadScene(Loader.Scene.GameScene);
+        });
+        quitBtn.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+
 }
