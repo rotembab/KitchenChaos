c7ea19144f3f87744f676c5eac7a5a65f09ec60e
Rotem Babani <rotembabmen@gmail.com>
2025-02-22 21:13:38 +0200

    Added countdown animation and sound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -75,6 +75,12 @@ public class SoundManager : MonoBehaviour
     {
         PlaySound(audioClipRefsSO.footstep,position,  volume);
     }
+
+
+    public void PlayCountdownSound()
+    {
+        PlaySound(audioClipRefsSO.warning,Vector3.zero);
+    }
     
     public void ChangeVolume()
     {
--- Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class GameStartCountdownUI : MonoBehaviour
 {
+ private const string NUMBER_POPUP = "NumberPopup";
+ 
  [SerializeField] private TextMeshProUGUI countdownText;
-
+ private Animator animator;
+ private int lastCountdownNumber;
+ 
+ private void Awake()
+ {
+  animator = GetComponent<Animator>();
+ }
+ 
  private void Start()
  {
   GameManager.Instance.OnGameStateChanged += HandleGameStateChanged;
@@ -32,6 +41,13 @@ public class GameStartCountdownUI : MonoBehaviour
 
  private void Update()
  {
-  countdownText.SetText(Mathf.Ceil(GameManager.Instance.GetCountdownToStartTimer()).ToString());
+  int countdownNumber = Mathf.CeilToInt(GameManager.Instance.GetCountdownToStartTimer());
+  countdownText.SetText(countdownNumber.ToString());
+  if (countdownNumber != lastCountdownNumber)
+  {
+   animator.SetTrigger(NUMBER_POPUP);
+   lastCountdownNumber = countdownNumber;
+   SoundManager.Instance.PlayCountdownSound();
+  }
  }
 }
